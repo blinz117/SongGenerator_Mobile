2b761a136420ca37bdf20cd987c2be6163ed724d
Bryan Lindsey <b-linz117@homail.com>
2013-08-16 19:17:20 -0400

    Added spinners to allow selecting a time signature.
    Added basic chord progression generation 
    	-shows scale degree... no modifiers, like Major, minor, 7th, etc. yet)
    
    Added basic rhythm creation
    	-shows an array containing durations in half beats, with rests as
    negative values
    	-based on time signature
    	-currently generates 2 rhythms.

--- src/com/blinz117/songgenerator/MainActivity.java
+++ src/com/blinz117/songgenerator/MainActivity.java
@@ -6,11 +6,15 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.*;
 import android.widget.*;
+import android.widget.AdapterView.OnItemSelectedListener;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnItemSelectedListener{
 
-	TextView songStructureView;
+	Spinner timeSigNumSpin;
+	Spinner timeSigDenomSpin;
 	Button songGenButton;
+	TextView songStructureView;
+	
 	SongWriter songWriter;
 	Song currSong;
 	
@@ -19,9 +23,25 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		songStructureView = (TextView)findViewById(R.id.songStructure);
+		// Set up adapter and listener for spinners
+		timeSigNumSpin = (Spinner) findViewById(R.id.timeSigNum);
+		ArrayAdapter<CharSequence> numAdapter = ArrayAdapter.createFromResource(this,
+		        R.array.timeSig_array, android.R.layout.simple_spinner_item);
+		numAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		timeSigNumSpin.setAdapter(numAdapter);
+		timeSigNumSpin.setOnItemSelectedListener(this);
+		
+		timeSigDenomSpin = (Spinner) findViewById(R.id.timeSigDenom);
+		ArrayAdapter<CharSequence> denomAdapter = ArrayAdapter.createFromResource(this,
+		        R.array.timeSig_array, android.R.layout.simple_spinner_item);
+		denomAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		timeSigDenomSpin.setAdapter(denomAdapter);
+		timeSigDenomSpin.setOnItemSelectedListener(this);
+		
 		songGenButton = (Button)findViewById(R.id.songGen);
 		songGenButton.setOnClickListener(onSongGenerate);
+		songStructureView = (TextView)findViewById(R.id.songStructure);
+		
 		songWriter = new SongWriter();
 		currSong = null;
 	}
@@ -37,8 +57,34 @@ public class MainActivity extends Activity {
 		public void onClick(View view)
 		{
 			Song currSong = songWriter.writeSong();
-			songStructureView.setText(currSong.vStructure.toString());
+			String displayString = "Time Signature: ";
+			displayString += songWriter.getTimeSigNumerator() + "/" + songWriter.getTimeSigDenominator();
+			displayString = displayString + "\n" + currSong.vStructure.toString();
+			displayString = displayString + "\nVerse: " + currSong.verseChords;
+			displayString = displayString + "\nChorus: " + currSong.chorusChords;
+			displayString = displayString + "\nBridge: " + currSong.bridgeChords;
+			displayString += "\nRhythm1: " + currSong.rhythm1;
+			displayString += "\nRhythm2: " + currSong.rhythm2;
+			
+			
+			songStructureView.setText(displayString);
 		}
 	};
+
+	@Override
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id)
+	{
+		Spinner spinner = (Spinner) parent;
+		String value = parent.getItemAtPosition(pos).toString();
+		if (spinner.getId() == R.id.timeSigNum)
+			songWriter.setTimeSigNumerator(Integer.parseInt(value));
+		else if (spinner.getId() == R.id.timeSigDenom)
+			songWriter.setTimeSigDenominator(Integer.parseInt(value));
+    }
+
+	@Override
+    public void onNothingSelected(AdapterView<?> parent) {
+		//Nothing to do as far as I can tell
+	}
 
 }
--- src/com/blinz117/songgenerator/SongWriter.java
+++ src/com/blinz117/songgenerator/SongWriter.java
@@ -1,21 +1,58 @@
 package com.blinz117.songgenerator;
 
 import java.util.Random;
-import java.util.Vector;
+import java.util.ArrayList;
 
 public class SongWriter {
 
-	public enum SongPart{
-		VERSE, CHORUS, BRIDGE
+	// these describe the intervals (in semi-tones) between the notes
+	// in each scale
+	static final int[] MAJORSCALE = {2, 2, 1, 2, 2, 2, 1};
+	static final int[] NATURALMINROSCALE = {2, 1, 2, 2, 1, 2, 2};
+	static final int[] HARMONICMINORSCALE = {2, 1, 2, 2, 1, 3, 1};
+	
+	static final double[] CHORDPROBS = {5.0, 1.5, 1.5, 4.0, 5.0, 1.5, 0.25};
+	static final int NUMCHORDS = CHORDPROBS.length;
+	int probSums = 0;
+	
+	public enum SongPart { VERSE, CHORUS, BRIDGE }
+	
+	// Define the pitches present in the "Western" system
+	// (forgive my ignorance on naming conventions)
+	public enum Pitch {C, C_SHARP, D, D_SHARP, E, F, F_SHARP, G, G_SHARP, A, A_SHARP, B }
+	Pitch[] PITCHES;
+	static final int NUMPITCHES = 12;
+	
+	Random randGen;
+	
+	Pitch mCurrKey;
+	int mTimeSigNumer;
+	int mTimeSigDenom;
+	
+	public SongWriter() 
+	{
+		// initialize a few things
+		for (int ndx = 0; ndx < NUMCHORDS; ndx++)
+		{
+			probSums += CHORDPROBS[ndx];
+		}
+		
+		PITCHES = Pitch.values();
+		
+		randGen = new Random();
+		
+		mCurrKey = null;
+		mTimeSigNumer = 0;
+		mTimeSigDenom = 0;
 	}
 	
-	public enum Pitch{
-		C, C_SHARP, D, D_SHARP, E, F, F_SHARP, G, G_SHARP, A, A_SHARP, B
-	}
+	public Pitch getKey() {return mCurrKey; }
+	public int getTimeSigNumerator() {return mTimeSigNumer; }
+	public int getTimeSigDenominator() {return mTimeSigDenom; }
 	
-	
-	public SongWriter(){
-	}
+	public void setKey(Pitch newKey) { mCurrKey = newKey; }
+	public void setTimeSigNumerator(int newNumer) { mTimeSigNumer = newNumer; }
+	public void setTimeSigDenominator(int newDenom) { mTimeSigDenom = newDenom; }
 	
 	public Song writeSong()
 	{
@@ -24,14 +61,74 @@ public class SongWriter {
 		return masterpiece;
 	}
 	
-	public class Song{
-		Vector<SongPart> vStructure;
+	public class Song
+	{
 		Random randGen;
+		ArrayList<SongPart> vStructure;
+		//Random randGen;
 		double[] baseProbs = {0.6, 0.3, 0.1};
+		
+		ArrayList<Integer> verseChords;
+		ArrayList<Integer> chorusChords;
+		ArrayList<Integer> bridgeChords;
+		
+		ArrayList<Integer> rhythm1;
+		ArrayList<Integer> rhythm2;
 		
 		public Song(){
 			randGen = new Random();
-			vStructure = new Vector<SongPart>();
+			//randGen = new Random();
+			vStructure = new ArrayList<SongPart>();
+			
+			verseChords = null;
+			chorusChords = null;
+			bridgeChords = null;
+			
+			rhythm1 = null;
+			rhythm2 = null;
+		}
+		
+		public ArrayList<Integer> generateChordProgression()
+		{
+			ArrayList<Integer> chordProg = new ArrayList<Integer>();
+			// make it an even number of chords for now
+			int numChords = 4*(randGen.nextInt(3) + 1);
+			for (int chord = 0; chord < numChords; chord++)
+			{
+				int nextChord = pickNdxByProb(CHORDPROBS) + 1;
+				chordProg.add(nextChord);
+			}
+			return chordProg;			
+		}
+		
+		public ArrayList<Integer> generateRhythm()
+		{
+			int numHalfBeats = mTimeSigNumer * 2;
+			ArrayList<Integer> rhythm = new ArrayList<Integer>();
+			
+			int note = 0;
+			while (note < numHalfBeats)
+			{
+				// generate a crappy probability spread
+				double[] probs = new double[numHalfBeats - note];
+				for (int prob = 0; prob < probs.length; prob++)
+				{
+					probs[prob] = (double)(probs.length - prob);
+				}
+				int numBeats = pickNdxByProb(probs) + 1;
+				if (numBeats < 0)
+					continue;
+				
+				note += numBeats;
+				
+				// small chance to be negative (a rest)
+				double restChance = randGen.nextDouble();
+				if (restChance < 0.2)
+					numBeats *= -1;
+				
+				rhythm.add(numBeats);
+			}
+			return rhythm;
 		}
 		
 		public void writeNewSong()
@@ -41,7 +138,7 @@ public class SongWriter {
 			double[] partProbs = baseProbs;
 			int numPartTypes = partProbs.length;
 			
-			int iNumParts = randGen.nextInt(3) + 4;
+			int iNumParts = randGen.nextInt(2) + 4;
 			SongPart nextPart = null;
 			
 			for (int iPart = 0; iPart < iNumParts; iPart++)
@@ -49,6 +146,7 @@ public class SongWriter {
 				double nextChoice = randGen.nextDouble();
 				double probSum = 0.0;
 				Boolean bContinueChecking = true;
+				// TODO: can probably refactor this to use the findNdxByProb function below
 				for (int ndx = 0; ndx < partProbs.length; ndx++)
 				{
 					if (bContinueChecking)
@@ -70,6 +168,55 @@ public class SongWriter {
 				}
 				vStructure.add(nextPart);
 			}
+			
+			rhythm1 = generateRhythm();
+			
+			// now generate chord progression for each segment
+			verseChords = generateChordProgression();
+			chorusChords = generateChordProgression();
+			bridgeChords = generateChordProgression();
+			
+			rhythm2 = null;
+			rhythm2 = generateRhythm();
+		} // writeNewSong
+		
+	} //class Song
+	
+	/*
+	 * returns an index into an array based on the probabilities in the array
+	 * The values in the array do not have to add up to 1. They will be scaled if not.
+	 * returns -1 if something goes wrong
+	 */
+	public int pickNdxByProb(double[] probs)
+	{
+		// make sure it has a length
+		if (probs.length < 1)
+			return -1;
+		
+		double probTotal = 0.0;
+		for (int prob = 0; prob < probs.length; prob++)
+		{			
+			//get sum total of all probabilities
+			probTotal += probs[prob];
 		}
-	}
-}
+		
+		//Random randGen = new Random();
+		double goalVal = randGen.nextDouble();
+		double curProbSum = 0.0;
+		for (int ndx = 0; ndx < probs.length; ndx++)
+		{			
+			//check probability to see if we found the one
+			curProbSum += probs[ndx]/probTotal;
+			if (goalVal < curProbSum)
+			{
+				// found the one we want
+				return ndx;
+			}
+		}
+		
+		//sentinel: should never get here
+		return -1;
+		
+	}//pickNdxByProb
+	
+} //class SongWriter
